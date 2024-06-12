85b14c2a753d7a13ecb61d562faef0cd0958b6d1
Ruslan <foyyui@gmail.com>
2024-06-12 18:10:27 +0300

    added CV

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,11 @@
 import SectionAbout from "./section-about";
 import SectionHome from "./section-home";
-import SectionSocials from "./section-socials";
 
 const Home = () => {
   return (
     <main>
       <SectionHome />
       <SectionAbout />
-      <SectionSocials />
     </main>  
   );
 }
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,6 +1,8 @@
 const Resume = () => {
   return (  
-    <></>
+    <section>
+
+    </section>
   );
 }
  
